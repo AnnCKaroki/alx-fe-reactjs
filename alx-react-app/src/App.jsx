584d0da4d370b5ffea76515d088cd6e91cbe7e2d
Ann <annkarokiadmissions@gmail.com>
2025-08-27 16:45:44 +0300

    feat: create profile card using props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import WelcomeMessage from './components/WelcomeMessage'
 import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
     <>
       <Header />
       <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <WelcomeMessage />
       <Footer />
     </>
