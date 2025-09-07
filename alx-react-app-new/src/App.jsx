cbc2bd15df24f554d52fc71b4a94d957adf3aa85
Ann <annkarokiadmissions@gmail.com>
2025-09-07 22:48:24 +0300

    Add component to app

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -4,15 +4,20 @@ import MainContent from './components/MainContent'
 import WelcomeMessage from './components/WelcomeMessage'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
+import Counter from './components/Counter'
+
 
 
 function App() {
+
+
 
 
   return (
     <>
       <Header />
       <MainContent />
+      <Counter />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <WelcomeMessage />
       <Footer />
