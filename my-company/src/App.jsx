0a6f0bb67af9084177765f978490349ee9558789
Ann <annkarokiadmissions@gmail.com>
2025-09-07 23:26:07 +0300

    implement routing

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,22 +1,29 @@
-import './App.css'
-import Header from './components/Header'
-import MainContent from './components/MainContent'
-import WelcomeMessage from './components/WelcomeMessage'
-import Footer from './components/Footer'
-import UserProfile from './components/UserProfile'
-
-
+import { BrowserRouter, NavLink, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 function App() {
 
-
   return (
-    <>
-      <Header />
-      <MainContent />
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-      <WelcomeMessage />
+    <BrowserRouter>
+
+      <Navbar />
+
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+
       <Footer />
-    </>
+
+    </BrowserRouter>
   )
 }
 
