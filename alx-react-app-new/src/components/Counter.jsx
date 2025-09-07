feb3c5a4b144cbc61766155ab4b97f3a88dad50e
Ann <annkarokiadmissions@gmail.com>
2025-09-07 22:41:26 +0300

    fix error

--- alx-react-app-new/src/components/Counter.jsx
+++ alx-react-app-new/src/components/Counter.jsx
@@ -1,14 +1,16 @@
-import React, { useState } from 'react';
-function Counter() {
-     const [count, setCount] = useState(0);
+import { useState } from 'react';
 
-     return (
-       <div>
-         <p>Current Count: {count}</p>
-         <button onClick={() => setCount(count + 1)}>Increment</button>
-         <button onClick={() => setCount(count - 1)}>Decrement</button>
-         <button onClick={() => setCount(0)}>Reset</button>
-       </div>
-     );
-   }
-export default Counter;
+export default function Counter() {
+
+    const [count, setCount] = useState(0);
+
+    return (
+        <div style={{ padding: "30px", border: "1px solid white", margin: "40px" }}>
+            <p style={{ fontSize: "30px" }}>Current count: <span style={{ color: "pink", fontWeight: "bold" }}>{count}</span></p>
+            <button style={{ margin: "12px", backgroundColor: "white", color: "black" }} onClick={() => setCount(count + 1)}>Increment</button>
+            <button style={{ margin: "12px", backgroundColor: "white", color: "black" }} onClick={() => setCount(count - 1)}>Decrement</button>
+            <button style={{ margin: "12px", backgroundColor: "white", color: "black" }} onClick={() => setCount(0)}>Reset</button>
+        </div>
+    )
+}
+
