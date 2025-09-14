9d3b85afa6e6d1f2a681816e80244cfdfe4d2447
Ann <annkarokiadmissions@gmail.com>
2025-09-14 22:40:41 +0300

    feat: update recipeList

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -2,17 +2,21 @@ import useRecipeStore from './recipeStore';
 import { Link } from 'react-router-dom';
 
 const RecipeList = () => {
+  const filteredRecipes = useRecipeStore(state => state.filteredRecipes);
   const recipes = useRecipeStore(state => state.recipes);
+
+  // Show all if no search, else show filtered
+  const list = filteredRecipes.length > 0 || useRecipeStore.getState().searchTerm
+    ? filteredRecipes
+    : recipes;
 
   return (
     <div>
       <h2>All Recipes</h2>
       <ul>
-        {recipes.map(recipe => (
+        {list.map(recipe => (
           <li key={recipe.id}>
-            <Link to={`/recipes/${recipe.id}`}>
-              {recipe.title}
-            </Link>
+            <Link to={`/recipes/${recipe.id}`}>{recipe.title}</Link>
           </li>
         ))}
       </ul>
