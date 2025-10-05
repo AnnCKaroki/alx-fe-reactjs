ead94ce245585d8c30e0a216a08ee1d2397ac604
Ann <annkarokiadmissions@gmail.com>
2025-10-05 18:31:37 +0300

    feat: add tailwindcss

--- recipe-sharing-platform/vite.config.js
+++ recipe-sharing-platform/vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // You can set it to 'media' or 'class' if needed
+  theme: {
+    extend: {},
+  },
   plugins: [react()],
 })
